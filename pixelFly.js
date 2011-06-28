9468d1a2142f5bd171f230dcb536bc20be3b8063
Roman Joost <roman@bromeco.de>
2011-06-28 21:29:07 +1000

    Allow to close the image and remove the fileuri from the localStore

--- pixelFly.js
+++ pixelFly.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
 
     $('#fileuri').change(function() {
         var fileuri = document.getElementById('fileuri').value
+        if (fileuri == '') {
+            delete localStorage[key];
+            return true;
+        }
         if (window['localStorage'] !== null) {
             if (olduri !== fileuri)
                 localStorage.setItem(key, fileuri)
@@ -16,7 +20,7 @@ $(document).ready(function () {
         if (!$('#Layer').length)
             var overlay = createOverlay(fileuri);
         else
-            $('#Layer img')[0].attr('src', fileuri);
+            $('#Layer img').attr('src', fileuri);
     });
 });
 
@@ -28,6 +32,7 @@ function createOverlay(fileuri) {
         $('body').append('<div id="'+id+'"><div id="LayerMenu" style="position: relative; top: 0; right: 0;"></div><img src="' + fileuri + '" /></div>')
         $('#LayerMenu').append('<input value="0" id="Xpos" readonly="true" />')
         $('#LayerMenu').append('<input value="' + opacity + '" id="Opacity" />')
+        $('#LayerMenu').append('<a href="#" id="Close">Close</a>')
         $('#Layer').draggable();
         $('#Layer').bind('dragstop', function(event, ui) {
             $('#Xpos').attr('value', ui.offset.left + ':' + ui.offset.top);
@@ -37,6 +42,12 @@ function createOverlay(fileuri) {
             $('#Layer img').css('opacity', $('#Opacity').attr('value'));
         });
         jQuery('#Opacity').trigger(jQuery.Event('change'));
+
+        $('#Close').click(function() {
+            $('#fileuri').attr('value', '');
+            jQuery('#fileuri').trigger(jQuery.Event('change'));
+            $('#' + id).empty();
+        });
     }
 }
 
