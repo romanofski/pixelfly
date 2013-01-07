2fbf8c3647b7110cc52c6c73f64732b8ae2cd6ca
Roman Joost <roman@bromeco.de>
2013-01-07 15:50:23 +1000

    Basic dragn drop

--- pixelFly.js
+++ pixelFly.js
@@ -67,10 +67,23 @@ function Layer(fileuri) {
     this.opacity = 0.5;
     this.id = 'Layer ' + fileuri;
     this.fileuri = fileuri;
+    this.x = 0;
+    this.y = 0;
+    this.img = null;
 }
 Layer.prototype.getHTMLNode = function() {
-    var img = document.createElement('img');
-    img.src = this.fileuri;
-    img.style.opacity = this.opacity;
-    return img;
+    this.img = document.createElement('img');
+    this.img.src = this.fileuri;
+    this.img.style.opacity = this.opacity;
+    this.img.style.position = 'fixed';
+    this.img.style.top = 0;
+    this.img.style.left = 0;
+    this.img.addEventListener('mousedown',
+            this.layerMouseDown,
+            true);
+    return this.img;
+}
+Layer.prototype.layerMouseDown = function(e) {
+    this.style.left = String(this.x + e.clientX) + 'px';
+    this.style.top = String(this.y + e.clientY) + 'px';
 }
