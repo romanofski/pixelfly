58e58f604887788e4a6bd0b3a69c48b1c4fe72a8
Roman Joost <roman@bromeco.de>
2013-01-10 11:27:55 +1000

    Stop processing events once the user releases the mouse.

--- pixelFly.js
+++ pixelFly.js
@@ -73,6 +73,9 @@ function Layer(fileuri) {
     this.fileuri = fileuri;
     this.x = 0;
     this.y = 0;
+    this.mouse_x = 0;
+    this.mouse_y = 0;
+    this.drag = false;
     this.img = null;
 }
 Layer.prototype.getHTMLNode = function() {
@@ -83,18 +86,28 @@ Layer.prototype.getHTMLNode = function() {
     this.img.style.top = 0;
     this.img.style.left = 0;
     this.img.addEventListener('mousedown',
-            this.layerMouseDown,
+            this.layerMouseDown.bind(this),
             true);
     this.img.addEventListener('mousemove',
-            this.layerMouseMove,
+            this.layerMouseMove.bind(this),
+            true);
+    this.img.addEventListener('mouseup',
+            this.layerMouseUp.bind(this),
             true);
     return this.img;
 }
 Layer.prototype.layerMouseMove = function(e) {
-    this.style.left = String(this.x + e.clientX - this.mouse_x) + 'px';
-    this.style.top = String(this.y + e.clientY - this.mouse_y) + 'px';
+    if (! this.drag) {
+        return
+    }
+    this.img.style.left = String(this.x + e.clientX - this.mouse_x) + 'px';
+    this.img.style.top = String(this.y + e.clientY - this.mouse_y) + 'px';
 }
 Layer.prototype.layerMouseDown = function(e) {
+    this.drag = true;
     this.mouse_x = e.clientX;
     this.mouse_y = e.clientY;
+}
+Layer.prototype.layerMouseUp = function(e) {
+    this.drag = false;
 }
