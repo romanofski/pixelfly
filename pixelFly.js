75d481ed07e246175c0f7b9952bdc76341404753
Roman Joost <roman@bromeco.de>
2013-01-13 15:08:38 +1000

    Delegate the fileuri selection to the fileselector

--- pixelFly.js
+++ pixelFly.js
@@ -1,50 +1,12 @@
 var FILEURI = 'fileuri';
-var dialog = new LayerDialog();
+
 
 window.addEventListener('load', function() {
-    if (dialog.layers.length == 0) {
-        var fileselector = new FileSelector();
+    window.pixelfly_dialog = new LayerDialog();
+    if (window.pixelfly_dialog.layers.length == 0) {
+        var layer = new Layer();
     }
-    dialog.render();
 });
-
-
-/* FileSelector
- */
-function FileSelector() {
-    this.fileuri = null;
-    this._input_id = 'fileuri';
-    if (!this.fileuri)
-        this.render();
-}
-FileSelector.prototype.handleChange = function(input, e) {
-    if (input == undefined) {
-        return false
-    }
-
-    var file = input.files[0];
-    if (file == undefined) {
-        return false
-    }
-
-    var fileuri = window.URL.createObjectURL(file);
-    var layer = new Layer(fileuri);
-    dialog.appendLayer(layer);
-    return true;
-}
-FileSelector.prototype.render = function() {
-    var body = document.getElementsByTagName('body')[0];
-    var input = document.createElement('input');
-    input.type = 'file';
-    input.name = this._input_id;
-    input.id = this._input_id;
-    body.appendChild(input);
-
-    input.addEventListener(
-            'change',
-            this.handleChange.bind(this, input),
-            true);
-}
 
 
 /* Layerdialog
@@ -52,35 +14,79 @@ FileSelector.prototype.render = function() {
 function LayerDialog() {
     this.layers = new Array();
     this._domid = 'pixelfly.layerdialog';
-}
-LayerDialog.prototype.render = function() {
-    var body = document.getElementsByTagName('body')[0];
     this._layerMenu = document.createElement('ul');
     this._layerMenu.id = this._domid;
+
+    var body = document.getElementsByTagName('body')[0];
     body.appendChild(this._layerMenu);
 }
 LayerDialog.prototype.appendLayer = function(layer) {
     this.layers.push(layer);
-    this._layerMenu.appendChild(layer.getHTMLNode());
+    this._layerMenu.appendChild(layer.html_container);
+}
+
+
+/* FileSelector
+ */
+function FileSelector(layer) {
+    this.layer = layer;
+    this.fileuri = null;
+    this._input_id = 'fileuri';
+}
+FileSelector.prototype.handleChange = function(input, e) {
+    var file = input.files[0];
+    if (file == undefined) {
+        return false
+    }
+
+    this.fileuri = window.URL.createObjectURL(file);
+    this.layer.update();
+    return true;
+}
+FileSelector.prototype.select_file = function() {
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.name = this._input_id;
+    input.id = this._input_id;
+
+    input.addEventListener(
+            'change',
+            this.handleChange.bind(this, input),
+            true);
+    return input;
 }
 
 
 /* Layer
  */
-function Layer(fileuri) {
+function Layer() {
     this.opacity = 0.5;
-    this.id = 'Layer ' + fileuri;
-    this.fileuri = fileuri;
+    this.id = 'Layer 1';
     this.x = 0;
     this.y = 0;
     this.mouse_x = 0;
     this.mouse_y = 0;
     this.drag = false;
+
     this.img = null;
+
+    this.html_container = document.createElement('li');
+
+    this.fileselector = new FileSelector(this);
+    if (! this.fileselector.fileuri) {
+        var input = this.fileselector.select_file();
+        this.html_container.appendChild(input);
+        window.pixelfly_dialog.appendLayer(this);
+    }
 }
+Layer.prototype.update = function() {
+    this.html_container.appendChild(this.getHTMLNode());
+}
+/* Re-render the layer (e.g. fileuri has changed)
+ */
 Layer.prototype.getHTMLNode = function() {
     this.img = document.createElement('img');
-    this.img.src = this.fileuri;
+    this.img.src = this.fileselector.fileuri;
     this.img.style.opacity = this.opacity;
     this.img.style.position = 'fixed';
     this.img.style.top = 0;
